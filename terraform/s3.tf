4662685adca79627094e07006567f2e1dc9a80d2
aguillement <adrien.guillement@gmail.com>
2023-01-19 14:39:57 +0000

    Add principal policy to S3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,3 +14,19 @@ resource "aws_s3_bucket_website_configuration" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.bucket_s3.id
 }
+
+data aws_iam_policy_document policy {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket_s3.arn}/*"]
+  }
+}
+
+resource aws_s3_bucket_policy bucket_policy {
+  bucket = aws_s3_bucket.bucket_s3.id
+  policy = data.aws_iam_policy_document.policy.json
+}
