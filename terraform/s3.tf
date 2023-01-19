5bf82d5c94a3aa9d89dec17ec33dddfb96149206
aguillement <adrien.guillement@gmail.com>
2023-01-19 14:26:16 +0000

    Add hugo steps

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,10 +2,15 @@
 resource "aws_s3_bucket" "bucket_s3" {
   bucket = var.bucket_name
 }
+
 resource "aws_s3_bucket_website_configuration" "example" {
   bucket = aws_s3_bucket.bucket_s3.bucket
 
   index_document {
     suffix = "index.html"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket_s3.id
 }
